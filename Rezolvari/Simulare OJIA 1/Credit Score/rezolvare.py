6e666ca7169bd9396d233334a11ccf9c030777b6
David_181 <davidgavriloiu07@gmail.com>
2025-09-30 13:09:09 +0300

    Rezolvari

---
+++ Rezolvari/Simulare OJIA 1/Credit Score/rezolvare.py
@@ -0,0 +1,4 @@
+import os
+print("Current working directory:", os.getcwd())
+import pandas as pd
+import numpy as np 
--- Rezolvari/Simulare OJIA 1/Livrare pachete/rezolvare.py
+++ Rezolvari/Simulare OJIA 1/Livrare pachete/rezolvare.py
@@ -1,3 +1,35 @@
+'''Problema Prezicerii Livrării Pachetelor la Timp
+În logistica modernă, livrarea la timp a pachetelor este esențială pentru satisfacția clienților și eficiența operațională. Dezvoltați un model care să prezică dacă un pachet va fi livrat la timp (on_time = 1) sau întârziat (on_time = 0).
+
+Veți antrena modelul folosind setul de date de antrenament furnizat și apoi veți genera predicții pentru un set de date ne-etichetat.
+
+Descrierea Setului de Date
+Setul de Date de Antrenament (train_data.csv) conține următoarele coloane:
+
+id (număr natural): Numărul care identifică livrarea - număr natural unic.
+distance_km (0.5-700): Distanța de la depozit până la destinația de livrare (în kilometri). Distanțele mai scurte indică, în general, o probabilitate mai mare ca livrarea să fie la timp.
+package_weight_kg (0.5-150): Greutatea pachetului (în kilograme). Pachetele mai ușoare sunt, de obicei, livrate mai rapid.
+traffic_level (1-14): Un nivel al condițiilor de trafic (tip intreg, 1 - trafic redus, 14 - trafic aglomerat maxim).
+on_time: O variabilă binară în care 1 indică faptul că pachetul a fost livrat la timp, iar 0 indică întârzierea.
+Setul de Date pentru Predicție (test_data.csv):
+
+Conține 50 de eșantioane cu aceleași caracteristici (id, distance_km, package_weight_kg și traffic_level) ca setul de antrenament, dar fără coloana on_time.
+Modelul vostru va genera predicții pentru aceste eșantioane.
+Subtask-uri
+Subtask 1 (20p): mean_traffic_level - reprezentând media nivelurilor de trafic din setul de date pentru predicție, precizie de 2 decimale
+Subtask 2 (20p): std_traffic_level - reprezentând deviația standard a aceluiași câmp (nivelul de trafic), cu precizie de 2 decimale
+Subtask 3 (60p): on_time - cu predicțiile modelului vostru, 1 pentru livrare la timp, respectiv 0 pentru întârziere.
+Rezultatul așteptat:
+Un fisier csv output.csv care să includă următoarele 3 coloane:
+
+subtaskID - reprezintă numărul subtaskului (1, 2 sau 3)
+datapointID - care se referă la coloana id din test_data.csv
+answer - răspunsul corespunzător datapointului pentru subtaskul respectiv
+Notă: Pentru subtask-urile 1 și 2, la care se cere un singur răspuns pentru tot setul de date, afișați o singură linie a cărei datapointID să fie 1.
+
+Trimiteți un singur csv care să conțină răspunsurile pentru toate subtask-urile pe care le-ați rezolvat. Pentru a vedea un exemplu, descărcați fișierul sample_output.csv'''
+
+
 import os
 print("Current working directory:", os.getcwd())
 import pandas as pd
